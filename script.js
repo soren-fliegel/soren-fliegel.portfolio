f5b9ea0333e291b92e2dd6a0bd9ffd6a6647c358
sorenfliegel <sorebear15@gmail.com>
2023-02-26 00:00:16 -0800

    apifix?

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
 btn.addEventListener("click", () => {
     let inpWord = document.getElementById("inp-word").value;
-    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/${inpWord}')
+    fetch(url + inpWord)
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
